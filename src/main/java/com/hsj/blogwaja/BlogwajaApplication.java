511a88b9079b0455c5cf0af5ba998adddf6f9643
Waja <2479486016@qq.com>
2019-06-03 06:45:47 +0800

    又改回来了

--- src/main/java/com/hsj/blogwaja/BlogwajaApplication.java
+++ src/main/java/com/hsj/blogwaja/BlogwajaApplication.java
@@ -3,21 +3,13 @@ package com.hsj.blogwaja;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.WebApplicationInitializer;
 
 /**
  * @author waja
  */
 @MapperScan("com.hsj.blogwaja.dao")
 @SpringBootApplication
-public class BlogwajaApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
-
-        return application.sources(BlogwajaApplication.class);
-    }
+public class BlogwajaApplication {
     public static void main(String[] args) {
         SpringApplication.run(BlogwajaApplication.class, args);
     }
