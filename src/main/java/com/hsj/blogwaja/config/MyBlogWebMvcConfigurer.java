92f8f3019509cb9c00ab3c6bb16a7c7ce534bc4e
Waja <2479486016@qq.com>
2019-06-04 06:04:14 +0800

    换来了首诗

--- src/main/java/com/hsj/blogwaja/config/MyBlogWebMvcConfigurer.java
+++ src/main/java/com/hsj/blogwaja/config/MyBlogWebMvcConfigurer.java
@@ -21,4 +21,5 @@ public class MyBlogWebMvcConfigurer implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/upload/**").addResourceLocations("file:" + Constants.FILE_UPLOAD_DIC);
     }
+
 }
